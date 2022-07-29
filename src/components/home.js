0aef9f92cca77e3e8099e976f0a0edd496d42d36
Sceptre <mbuguag026@gmail.com>
2022-07-29 17:13:54 +0300

    updates

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../images/logo4.png';
 import logo1 from '../images/logo7.png';
+import hero1 from '../images/hero1.jpg';
+import hero2 from '../images/hero2.jpg';
+import hero3 from '../images/hero3.jpg';
+import hero4 from '../images/hero4.jpg';
+import hero5 from '../images/hero5.jpg';
+import hero6 from '../images/hero6.jpg';
+import hero7 from '../images/hero7.jpg';
 
 class Home extends Component{
     render(){
@@ -27,7 +34,28 @@ class Home extends Component{
                     <span>Scale</span>
                 </div>
 
-                <div className='blank'></div>
+                <div className='home'>
+                    <div className='home__section'>
+                        <div className='hero1'>
+                            <img src={hero4} alt='hero2' />
+                        </div>
+                        <div className='hero2'>
+                            <img src={hero2} alt='hero3' />
+                        </div>
+                        <div className='hero3'>
+                            <img src={hero3} alt='hero3' />
+                        </div>
+                        <div className='home__desc'>
+                            <h1>Thousands of customers trust our company</h1>
+                            <p style={{paddingBottom: '20px'}}>A premier Shopify agency specializing in management and ecommerce marketing solutions for global Shopify brands.</p>
+                            <p>We specialize in delivering Shopify ecommerce solutions that outperform our client's expectations across <span>Shopify management</span>, and <span> Shopify marketing strategy.</span></p>
+                            <button>Let's Talk</button>
+                        </div>
+                    </div>
+                    <div className='mid__section'>
+
+                    </div>
+                </div>
 
                 <div className='footer'>
                     <div className='footer__left'>
